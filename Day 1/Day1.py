01d8a8a9fb1d5f4fef3a3ce7bea34abac15ad372
Seifehab11-8 <160287704+Seifehab11-8@users.noreply.github.com>
2024-06-18 22:45:24 +0300

    Update Day1.py

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,1 +1,8 @@
+# greeting
+print("Welcome to the band name generator")
+# variable assigning
+nameOfCity = input("What is the name of the city you grew up in ?\n");
+nameOfPet = input("What is the name of your pet ?\n");
+# output
+print("your band name could be "+nameOfCity+" "+nameOfPet)
 
