ad171662df54e752ec297875afa7cb2d7b08c7f3
Seifehab11-8 <160287704+Seifehab11-8@users.noreply.github.com>
2024-06-19 15:52:46 +0300

    Update Day2.py

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -1,1 +1,14 @@
+#greeting
+print("Welcome to the tip calculator!")
+#recieving total bill
+total_Bill = float(input("What is your total bill? $"))
+#calculate the tip amount
+total_tip = float(input("How much tip do you like to give (10% , 12%, 15%) $")) * (total_Bill/100)
+numOfPeople = int(input("How many people to split the bill? $"))
 
+total = total_tip + total_Bill
+total_per_person = total/numOfPeople
+
+total_per_person = round(total_per_person ,2) # rounds to 2 decimal places
+# f-string to make automatic casting to string type
+print(f"Each person should pay: ${total_per_person}")
