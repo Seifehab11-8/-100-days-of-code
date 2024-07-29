f39cb6eadd5a26d11846e1bd225c797cf4a8f649
Seifehab11-8 <160287704+Seifehab11-8@users.noreply.github.com>
2024-07-29 22:44:13 +0300

    Update blind_bidding.py

--- Day 9/blind_bidding.py
+++ Day 9/blind_bidding.py
@@ -1,1 +1,16 @@
+bidders = {}
+flag = "yes"
+
+print("WELCOME TO THE SILENT BIDDING")
+while flag == "yes":
+    name = input("What is your name?\n")
+    bidding_value = int(input("What is your bidding amount? $"))
+    bidders[name] = bidding_value
+    flag = input("Would anyone else like to bid? (yes/no)\n")
+
+bid_values = list(bidders.values())
+max_bid = max(bid_values)
+name_list = list(bidders.keys())
+winner_name = name_list[bid_values.index(max_bid)]
+print(f"The winner is {winner_name} with a bid of {max_bid}$")
 
